afd2e31fc3427a4f6289dd7fb46a7d1dbbcbbf39
Ahmad Bacchus <abacchus@outlook.com>
2021-08-28 00:38:26 -0400

    Update checkOut.groovy

--- vars/checkOut.groovy
+++ vars/checkOut.groovy
@@ -1,3 +1,14 @@
 def call(){
-    sh "echo Hello from Checkout"
+
+    sh "echo " + checkout()
+    sh "echo " + getTag()
+    
+}
+
+def checkout(){
+    return "echo Hello from Checkout"
+}
+
+def getTag(){
+    return "echo Hello from getTag"
 }
