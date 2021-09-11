49355296046c4a1942c7fd7ac02c4dd3e55da0ca
Ahmad Bacchus <abacchus@outlook.com>
2021-09-11 00:18:03 -0400

    remove all environment from echo

--- vars/getDependencies.groovy
+++ vars/getDependencies.groovy
@@ -2,9 +2,5 @@
 
 def call(){
     sh "echo this is from the first stage!"
-    sh "echo " + printParams()
-}
 
-def printParams() {
-  env.getEnvironment().each { name, value -> println "Name: $name -> Value $value" }
 }
